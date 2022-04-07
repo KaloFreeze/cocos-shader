f21acd07bff652742427a97be4adeca2f53cddb1
onion <540650571@qq.com>
2022-04-07 14:47:08 +0800

    v1.0

--- assets/Scene/zhuiguang/zhuiguang-scene.ts
+++ assets/Scene/zhuiguang/zhuiguang-scene.ts
@@ -17,17 +17,18 @@ export default class NewClass extends cc.Component {
     renderSpr: cc.Sprite = null;
     // LIFE-CYCLE CALLBACKS:
     texture1: cc.RenderTexture;
-    texture2: cc.RenderTexture;
+    // texture2: cc.RenderTexture;
     // onLoad () {}
 
     start () {
-        this.renderSpr.getMaterial(0).setProperty("wh_ratio", this.bg.node.width / this.bg.node.height)
+        this.renderSpr.getMaterial(0).setProperty("wh_ratio", cc.winSize.width / cc.winSize.height)
         this.node.on(cc.Node.EventType.TOUCH_START, this.toucheEvent, this);
         this.node.on(cc.Node.EventType.TOUCH_MOVE, this.toucheEvent, this);
         this.texture1 = new cc.RenderTexture();
-        this.texture2 = new cc.RenderTexture();
+        // this.texture2 = new cc.RenderTexture();
         this.texture1.initWithSize(cc.winSize.width, cc.winSize.height);
-        this.texture2.initWithSize(cc.winSize.width, cc.winSize.height);
+        // this.texture1.initWithSize(this.node.width, this.node.height);
+        // this.texture2.initWithSize(cc.winSize.width, cc.winSize.height);
 
         this.renderCamera.targetTexture = this.texture1;
         // this.mainCamera.targetTexture = this.texture2;
@@ -36,12 +37,20 @@ export default class NewClass extends cc.Component {
 
     toucheEvent(evt: cc.Event.EventTouch) {
         let pos = evt.getLocation();
-        this.renderSpr.getMaterial(0).setProperty("center", [pos.x / this.bg.node.width, pos.y / this.bg.node.height])
+        this.renderSpr.getMaterial(0).setProperty("center", [pos.x / cc.winSize.width, pos.y / cc.winSize.height])
     }
 
     update (dt) {
         let spriteFrame = new cc.SpriteFrame();
         spriteFrame.setTexture(this.texture1);
         this.renderSpr.spriteFrame = spriteFrame;
+    }
+
+    onSliderSize(slider:cc.Slider) {
+        this.renderSpr.getMaterial(0).setProperty("radius", slider.progress)
+    }
+
+    onSliderBlur(slider:cc.Slider) {
+        this.renderSpr.getMaterial(0).setProperty("blur", slider.progress * 0.2)
     }
 }
